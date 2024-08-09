859c55bcc2abd1dd1f914adcefbc27305fb364ae
unknown <leojoamr01@gmail.com>
2024-08-09 22:48:38 +0800

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.scss'
+function Home() {
+  return (
+    <div className='body'>
+        <div className="background">
+            <div className="leaf"></div>
+            <div className="leaf"></div>
+            <div className="gold"></div>
+
+           <div className="info">
+              <div className="name">Leandro</div>
+              <div className="and">AND</div>
+              <div className="name">Leovy</div>
+              <div className="inv">INVITE YOU TO BE PART OF OUR</div>
+              <div className="inv">CELEBRATION OF LOVE AS</div>
+              <div className="inv">WE UNITE IN MARRIAGE</div>
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
